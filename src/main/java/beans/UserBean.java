01b3ff01cedc98708de37f09c0fb05c71976622a
harimanasa <harimanasa17@gmail.com>
2019-11-09 22:34:57 -0800

    Create UserBean.java

---
+++ src/main/java/beans/UserBean.java
@@ -0,0 +1,37 @@
+package beans;
+
+public class UserBean {
+
+	
+	String pocname;
+	String address;
+	String phoneNumber;
+	String location;
+	
+	public String getPocname() {
+		return pocname;
+	}
+	public void setPocname(String pocname) {
+		this.pocname = pocname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	
+}
